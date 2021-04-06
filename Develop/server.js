f25b9381697bce7cac0ec6dcafc84968fa6ee024
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-04-06 02:26:49 -0500

    Finished base server.js file, created public folder

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on Port 3001'));
+})
