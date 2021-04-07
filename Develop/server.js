21e306525ff10b352f87544c13e112c35fe62d48
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-04-07 08:35:04 -0500

    made some minor changes in server.js after initializing for first time

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const routes = require('./controllers');
 const hbs = exphbs.create({ helpers });
 
 const app = express();
@@ -18,4 +20,4 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening on Port 3001'));
-})
+});
